0c7ccf6a2794cd19834c701988358a62e9c3ffdd
xuguining <xuguining@qq.com>
2023-05-09 16:00:38 +0800

    修改了语言方面的问题

--- src/i18n/lang.js
+++ src/i18n/lang.js
@@ -2,13 +2,13 @@ const messages = {
 	/* 英文 */
 	en: {
 		helpCenter: 'Help Center',
-		slogan: 'Sign in with a chen\'s digital Account to enjoy more services.',
+		slogan: 'Sign in with a ning\'s digital Account to enjoy more services.',
 		login: {
 			msg: 'Verification code',
 			pass: 'Sign in with password',
 			policy: 'Privacy Notice',
-			backup: 'Copyright © chen\'s digital practice project',
-			contact: 'Tel：13710809956',
+			backup: 'Copyright © ning\'s digital practice project',
+			contact: 'Tel：13727784929',
 			p_phone: 'Phone or account',
 			p_pass: 'code',
 			loginText: 'login',
@@ -18,13 +18,13 @@ const messages = {
 	/* Chinese Simple */
 	cs: {
 		helpCenter: '帮助中心',
-		slogan: '登录小陈数码帐号可享受更多的服务',
+		slogan: '登录阿凝数码帐号可享受更多的服务',
 		login: {
 			msg: '短信验证码登录',
 			pass: '密码登陆',
 			policy: '隐私政策',
-			backup: '© 小陈数码练习项目--',
-			contact: ' 联系方式：13710809956',
+			backup: '© 阿凝数码练习项目--',
+			contact: ' 联系方式：13727784929',
 			p_phone: '请输入手机号码或账号',
 			p_pass: '请输入验证码',
 			loginText: '登录',
@@ -34,13 +34,13 @@ const messages = {
 	/* traditional Chinese*/
 	tc: {
 		helpCenter: '幫助中心',
-		slogan: '登入小陳數碼帳號可享受更多服務',
+		slogan: '登入阿凝數碼帳號可享受更多服務',
 		login: {
 			msg: '簡訊驗證碼登入',
 			pass: '帳戶密碼登入',
 			policy: '隱私權政策',
-			backup: '© 小陳數碼練習項目--',
-			contact: '聯絡方式：13710809956',
+			backup: '© 阿凝數碼練習項目--',
+			contact: '聯絡方式：13727784929',
 			p_phone: '請輸入手機號碼或賬號',
 			p_pass: '請輸入驗證碼',
 			loginText: '登入',
